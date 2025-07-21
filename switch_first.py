e09a8c119bef8a220289fe1eb1daa27f74d85c68
Phil Mike Jones <philmikejones@gmail.com>
2025-07-21 21:38:57 +0100

    refactor switch code

--- switch_first.py
+++ switch_first.py
@@ -1,23 +1,20 @@
 rooms = {
-    'room1_first_entry' : True,
-    'room1_desc_first' : 'this is the first time you\'ve entered this room',
-	'room1_desc' : 'this is every other time you enter this room'
+    'room1' : {
+        'first_entry' : True,
+        'desc_first' : 'this is the first time you\'ve entered this room',
+        'desc' : 'this is every other time you enter this room'
+    }
 }
 
 def get_desc(room):
     global rooms
-    room_first_entry = room + "_first_entry"
-    if rooms[room_first_entry]:
-        desc = room + "_desc_first"
-        rooms.update({room_first_entry : False})
+    if rooms.get(room).get('first_entry'):
+        print('True')
+        rooms.get(room).update({'first_entry' : False})
+    elif not rooms.get(room).get('first_entry'):
+        print('False')
     else:
-        desc = room + "_desc"
-    
-    desc = rooms[desc]
-    return desc
+        print('Error somewhere')
 
-desc = get_desc(room = 'room1')
-print(desc)
-
-desc = get_desc(room = 'room1')
-print(desc)
+get_desc('room1')
+get_desc('room1')
